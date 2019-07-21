11ec0f38a9f0432fb840be2f0fdcecae2598ba8a
gkarumba <gachegua@gmail.com>
2019-07-21 19:57:28 +0300

    Add the user model

--- news/views.py
+++ news/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, Http404,HttpResponseRedirect
 import datetime as dt
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render,redirect
 from .models import Article
-from .forms import NewsLetterForm
+from .forms import NewArticleForm, NewsLetterForm
 from .email import send_welcome_email
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def welcome(request):
@@ -64,10 +66,26 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'all-news/search.html',{"message":message})
-
+    
+@login_required(login_url='/accounts/login/')
 def article(request,article_id):
     try:
         article = Article.objects.get(id = article_id)
-    except DoesNotExist:
+    except ObjectDoesNotExist:
         raise Http404()
     return render(request,"all-news/article.html", {"article":article})
+
+@login_required(login_url='/accounts/login/')
+def new_article(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = NewArticleForm(request.POST, request.FILES)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.editor = current_user
+            article.save()
+        return redirect('newsToday')
+
+    else:
+        form = NewArticleForm()
+    return render(request, 'new_article.html', {"form": form})
--- tribune/settings.py
+++ tribune/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     # 'news.apps.NewsConfig',
     'news',
     'bootstrap3',
+    'tinymce',
 ]
 
 MIDDLEWARE = [
